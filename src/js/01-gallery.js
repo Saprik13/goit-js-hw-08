8f574276419038b7adf5b646f7e5a2b5d889e019
Petro Sapronov <saprik13@gmail.com>
2022-08-04 23:00:48 +0300

    add player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(
+      image => `
+          <a class="gallery__item" href="${image.original}">
+          <img class="gallery__image"
+          src="${image.preview}" 
+          alt="${image.description}" />
+</a>
+  `
+    )
+    .join('');
+}
+const addImages = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('afterbegin', addImages);
+
+let lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
